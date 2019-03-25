a05c6e222084a9ccc604b76675d19c405ee3e948
F <f@rawtx.com>
2019-03-24 22:28:18 -0700

    add initial messages.

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ const httpServer = new http.Server(app);
 const io = socketio(httpServer);
 
 let boltheadCounter = 3;
+let messages: Message[] = [];
+const MAX_MESSAGES = 200; // don't keep more than this many messages in memory.
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -20,6 +22,8 @@ io.on('connection', function(socket) {
   boltheadCounter++;
   io.emit('updateBoltheadCounter', boltheadCounter);
 
+  socket.emit('initialMessages', messages);
+
   socket.on('disconnect', function() {
     console.log('a user disconnected');
 
@@ -28,8 +32,14 @@ io.on('connection', function(socket) {
   });
 
   socket.on('newMessage', function(msg: Message) {
+    console.log('adding message', msg);
     msg.settled = false;
     // SET INVOICE msg.invoice =
+    messages.push(msg);
+    if(messages.length > MAX_MESSAGES){
+      // inefficient and ugly.
+      messages.shift();
+    }
     io.emit('newMessage', msg);
   });
 });
